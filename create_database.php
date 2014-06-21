42a1803f9f01a3a9916d43b87d85334e4f663c81
Avitus27 <mervyn511@gmail.com>
2014-06-21 16:30:31 +0100

    Fix for create_database.php
    Worked around a problem where mySQL couldn't add 2 tables in one command
    via PHP

--- create_database.php
+++ create_database.php
@@ -11,20 +11,27 @@
 	`password` varchar(255) NOT NULL,
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (`id`),
-	UNIQUE KEY `username` (`username`),
-	)
-	CREATE TABLE IF NOT EXISTS `comments` (
+	UNIQUE KEY `username` (`username`)
+	)";
+	//Query the Database
+	$dbresult = mysqli_query($dbconnection, $sql);
+	if(!$dbresult)
+	{
+		die("Something went wrong! " . mysql_error($dbconnection));
+	}
+	
+	$sql = "CREATE TABLE IF NOT EXISTS `comments` (
 	`comment_id` int(11) NOT NULL AUTO_INCREMENT,
 	`username` varchar(255) NOT NULL,
 	`comment_text` varchar(255) NOT NULL,
 	PRIMARY KEY (`comment_id`)
-	)"
+	)";
 	
 	//Query the Database
 	$dbresult = mysqli_query($dbconnection, $sql);
 	if(!$dbresult)
 	{
-		die("Something went wrong! " . mysqli_error($dbresult));
+		die("Something went wrong! " . mysqli_error($dbconnection));
 	}
 	else
 		echo "Tables created! You can now delete this file and use the other files to register users (direct users to \"register.html\" to be registered), let users log in (using \"login.html\") and take comments (with \"comments.php\")";
